f6aff12f9f8264f5ee498a217d4448821d9ed645
dylanhitt <dylan.hitt1@gmail.com>
2022-06-06 22:11:15 +0000

    WIP: add base eks module with optional cloud watch group configuration

--- aws/eks/main.tf
+++ aws/eks/main.tf
@@ -1,6 +1,20 @@
+resource "aws_cloudwatch_log_group" "this" {
+  count = var.create_log_group != "" ? 1 : 0
+
+  name              = "/aws/eks/${var.cluster_name}/cluster"
+  retention_in_days = 7
+}
 
 resource "aws_eks_cluster" "this" {
-  name = var.name
+  depends_on = [aws_cloudwatch_log_group.this]
 
+  name     = var.name
+  role_arn = var.role_arn
+  version  = var.cluster_version
 
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = var.public_access
+    subnet_ids              = var.subnet_ids # need to default this to grabbing tow different default subnets in a VPC
+  }
 }
